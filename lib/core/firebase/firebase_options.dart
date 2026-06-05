99e87d70058567112fcdd1436d8d0a6d8fe301dd
Marco A. Lonardon Jr. <112179645+marcolonardon@users.noreply.github.com>
2026-06-05 17:01:21 -0300

    Feat: Colocando dados reais para conexao ao firebase

--- lib/core/firebase/firebase_options.dart
+++ lib/core/firebase/firebase_options.dart
@@ -2,29 +2,54 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
-  static const apiKey = String.fromEnvironment('FIREBASE_API_KEY');
+  static const apiKey = String.fromEnvironment(
+    'FIREBASE_API_KEY',
+    defaultValue: 'AIzaSyD2zHkd7shuM8xOROhYt24i4KIijgl9KXg',
+  );
+  static const webApiKey = String.fromEnvironment(
+    'FIREBASE_WEB_API_KEY',
+    defaultValue: 'AIzaSyB4m82IHSyb5NT5LV-YdZUsSu8KVCyhIOI',
+  );
   static const appId = String.fromEnvironment('FIREBASE_APP_ID');
-  static const androidAppId = String.fromEnvironment('FIREBASE_ANDROID_APP_ID');
+  static const androidAppId = String.fromEnvironment(
+    'FIREBASE_ANDROID_APP_ID',
+    defaultValue: '1:1047017686121:android:5a61dd50be9bba386a4ef8',
+  );
   static const iosAppId = String.fromEnvironment('FIREBASE_IOS_APP_ID');
-  static const webAppId = String.fromEnvironment('FIREBASE_WEB_APP_ID');
+  static const webAppId = String.fromEnvironment(
+    'FIREBASE_WEB_APP_ID',
+    defaultValue: '1:1047017686121:web:2d8049382e315b916a4ef8',
+  );
   static const messagingSenderId = String.fromEnvironment(
     'FIREBASE_MESSAGING_SENDER_ID',
+    defaultValue: '1047017686121',
   );
-  static const projectId = String.fromEnvironment('FIREBASE_PROJECT_ID');
+  static const projectId = String.fromEnvironment(
+    'FIREBASE_PROJECT_ID',
+    defaultValue: 'backstage-prototipo',
+  );
   static const authDomain = String.fromEnvironment('FIREBASE_AUTH_DOMAIN');
   static const storageBucket = String.fromEnvironment(
     'FIREBASE_STORAGE_BUCKET',
+    defaultValue: 'backstage-prototipo.firebasestorage.app',
   );
   static const measurementId = String.fromEnvironment(
     'FIREBASE_MEASUREMENT_ID',
+    defaultValue: 'G-461JBX219Y',
   );
   static const iosBundleId = String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID');
 
   static bool get isConfigured {
-    return apiKey.isNotEmpty &&
+    return apiKeyForCurrentPlatform.isNotEmpty &&
         appIdForCurrentPlatform.isNotEmpty &&
         messagingSenderId.isNotEmpty &&
         projectId.isNotEmpty;
+  }
+
+  static String get apiKeyForCurrentPlatform {
+    if (kIsWeb && webApiKey.isNotEmpty) return webApiKey;
+
+    return apiKey;
   }
 
   static String get appIdForCurrentPlatform {
@@ -47,7 +72,7 @@ class DefaultFirebaseOptions {
     }
 
     return FirebaseOptions(
-      apiKey: apiKey,
+      apiKey: apiKeyForCurrentPlatform,
       appId: appIdForCurrentPlatform,
       messagingSenderId: messagingSenderId,
       projectId: projectId,
